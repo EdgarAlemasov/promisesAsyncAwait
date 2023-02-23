6154e65fe4633588d5b1d568aa784554159e97e5
Edgar Alemasov <edgar.alemasov@gmail.com>
2023-02-23 12:06:30 +0500

    start task

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "jest": true,
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        'no-restricted-syntax': [
+            'error',
+            'LabeledStatement',
+            'WithStatement',
+            ],
+        'no-console': 'off',
+    }
+}
